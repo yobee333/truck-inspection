2d572477026e9473d0c86ec17f3a5152c158dd3a
yobee333 <bridgetbrevik@outlook.com>
2021-04-29 15:46:48 -0500

    Add files via upload

---
+++ Inspection/JS/main.js
@@ -0,0 +1,31 @@
+document.querySelector('#vin-button').addEventListener('click', getFetch)
+
+function getFetch(){
+    const vin = document.querySelector('input').value
+    const url = 'https://vpic.nhtsa.dot.gov/api/vehicles/DecodeVinExtended/'+ vin + '?format=json'
+
+fetch(url)
+     .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data)
+        document.querySelector('#year').innerText = data.Results[9].Value
+        let make = document.querySelector('#make').innerText = data.Results[6].Value
+        document.querySelector('#model').innerText = data.Results[8].Value
+        let series = document.querySelector('#series').innerText = data.Results[11].Value
+    })
+    
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
+
+//submit title and bullets for listing
+
+document.querySelector('#submit').addEventListener('click', fillInTheBlanks)
+
+function fillInTheBlanks(){
+    let title = ('We offer a ')
+}
+
+
+//[9]-year, [6]-make, [8]-model, [11]-series
